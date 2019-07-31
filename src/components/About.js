f360f0d40e8e7cfa97d6dcfbece54cbad56daf5f
ramygouiaa <ramigouia1990@gmail.com>
2019-07-31 23:15:10 +0200

    first commit react training: Routers

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas suscipit quaerat quos molestiae perspiciatis magnam molestias dignissimos ut incidunt! Veniam et aut laborum totam quam dolore deleniti, in a hic.</p>
+        </div>
+    )
+
+}
+
+export default About
---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contact = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas suscipit quaerat quos molestiae perspiciatis magnam molestias dignissimos ut incidunt! Veniam et aut laborum totam quam dolore deleniti, in a hic.</p>
+        </div>
+    )
+
+}
+
+export default Contact
