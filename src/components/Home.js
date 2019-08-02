659bb1f5ec071622c1e7fe527ead02fa8c3fe8e6
ramygouiaa <ramigouia1990@gmail.com>
2019-08-02 23:21:22 +0200

    fetching data from fake rest api with axios and render the data in home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, {Component}from "react";
+import axios from "axios";
 
-const Home = () => {
+class Home extends Component {
+    state = {
+        posts:[ ]
+    }
+    componentDidMount(){
+axios.get('https://jsonplaceholder.typicode.com/posts')
+.then((res)=>{
+    console.log(res.data); 
+    this.setState({
+        posts:res.data.slice(0,10)
+    })
+    
+})
+    }
+    render(){
+        const {posts} = this.state
+        const postsList = posts.length ? (
+            posts.map(post=>{
+                return (
+                    <div className= "post card" key = {post.id}>
+                        <div className="card-content"></div>
+                        <span className="card-title">{post.title}</span>
+                        <p>{post.body}</p>
+                    </div>
+                )
+            })
+        ): (
+            <div className="center">no posts yet</div>  
+
+        )
     return (
         <div className="container">
             <h4 className="center">Home</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas suscipit quaerat quos molestiae perspiciatis magnam molestias dignissimos ut incidunt! Veniam et aut laborum totam quam dolore deleniti, in a hic.</p>
+            {/* <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas suscipit quaerat quos molestiae perspiciatis magnam molestias dignissimos ut incidunt! Veniam et aut laborum totam quam dolore deleniti, in a hic.</p> */}
+            {postsList}
         </div>
     )
-
+}
 }
 
 export default Home
